ebbc35dd4b9b5bde657a3032939742c5e23485f8
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-07-09 15:25:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 x = 0;
 y = 0;
 screen_width = 0;
-screen_height =0;
+screen_height = 0;
 draw_apple = "";
-apple = "apple.png";
+apple = "";
 var speak_data = "";
 to_number  = "";
 
@@ -30,31 +30,35 @@ if(Number.isInteger(to_number)){
   draw_apple ="set";
 } else {
   document.getElementById("status").innerHTML = "The speech has not been recognized as a number";
-}
+}}
 
 function setup() {
  screen_width = window.innerWidth;
  screen_height = window.innerHeight;
- createCanvas(screen_width, screen_height-150);
+ canvas = createCanvas(screen_width, screen_height-150);
  canvas.position(0, 150) ;
 }
 
 function preload()
 {
-  loadImage("apple");
+  apple = loadImage("apple.png");
 }
 
 function draw() {
   if(draw_apple == "set")
   {
    
-    draw_apple = "";
+    
     for(var i = 1;  i <= to_number; i++)
     {
       x = Math.floor(Math.random() * 700);
       y = Math.floor(Math.random() * 400);
       image(apple, x, y, 50, 50);
+
       document.getElementById("status").innerHTML = to_number + " Apple is  drawn";
+      speak_data = to_number+"apple is drawn";
+      speak();
+      draw_apple = "";
     }
   }
 }
@@ -70,4 +74,3 @@ function speak(){
 }
 
 
-}
